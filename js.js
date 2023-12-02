8d7eb2259bba7f8bc6f262f292ab6ddfcc9166fa
NurMuhammad1988 <nurikyorov88@gmail.com>
2023-12-03 00:11:24 +0500

    my old lessons

---
+++ js.js
@@ -0,0 +1,231 @@
+// // let maxSpeed = 19;
+// // maxSpeed = 20;
+// // console.log(maxSpeed);
+
+// let age = 35; //data-type = number
+// let myName = "Nur"; //data-type = string
+// let bool = false; //data-type = boolean
+// let number; //data-type = undefind
+// // console.log(number);
+// // console.log(a); // data-type=null
+
+// //object
+// let person = {
+//     name: "Nur", //string
+//     age: 35, //number
+//     isMarried: false, //boolean isMarried: false or tree
+// };
+
+// console.log(person);
+
+// //array
+// let arr = ["phone", "mouse", "soap"];
+
+// console.log(arr); // [] qilibn arreyni ichidagilarni tanlab faqat keraklisiniham chiqarishn mumkun
+// arr[0] = "desktop";
+// console.log(arr);
+
+// let x = 5;
+// let y = 6;
+
+// console.log(x + 5);
+// console.log(x - 5);
+// console.log(x * 5);
+// console.log(x / 5);
+
+// let z = 15;
+
+// console.log(z / 7);
+
+// // x = x + 1;
+// x++;
+// console.log(x);
+
+// x--;
+// console.log(x);
+
+// x--;
+// console.log(x);
+
+// let a = 10;
+// let b = 10;
+
+// console.log(a == b);
+
+// console.log("10" === 10);
+
+// console.log(-4 / 0);
+
+// console.log("4" + 4);
+
+// // alert(1)
+// // alert('hello world')
+// // let answers = confirm('sizni yoshiz 35 mi')
+// // console.log(answers);
+
+// // prompt('what is your name?') //????????????????
+// let answers = prompt('what is your name');
+// console.log(answers);
+
+// let i = 0;// 2chi yoli
+// do {
+//     console.log(i);
+//     i++;
+// } while (i < 10);
+
+// let i = 0; //3 chi yoli
+// while (i < 10) {
+//     console.log(i);
+//     i++;
+// }
+
+// let answers = []
+// let questions = [
+//     'What is your name',
+//     'What is your phone',
+//     'What how old are you',
+// ]
+
+// for (let i = 0; i < questions.length; i++){
+//     answers [i] = prompt(questions[i])
+// }
+
+// console.log(answers);
+
+// let age = 19;
+// if (age > 18) {
+//     alert("Hush kelipsiz");
+// } else {
+//     alert("Hali vaqtli");
+// }
+
+// let age = prompt("Yoshingiz nechchida");
+// if (age > 18) {
+//     alert("Hush kelipsiz");
+// } else {
+//     alert("Hali vaqtli");
+// }
+
+// let age = prompt('Yoshingiz nechchida');
+
+// if (age < 18) {
+//     alert('Hali vaqtli');
+
+// } else if (age > 80) {
+//     alert('Allaqachon kech');
+
+// } else {
+//     alert('Hali vaqtli');
+// }
+
+// let age = prompt("Yoshingiz nechchida");
+
+// if (age < 18) {
+//     alert("Hali vaqtli");
+// } else if (age > 80) {
+//     alert("Allaqachon kech");
+// } else {
+//     alert("Hush kelipsiz");
+// }
+
+// let age = +prompt("Yoshingiz nechchida");
+
+// switch (age) {
+//     case 18:
+//         alert("Sizni yoshiz 18 da");
+//         break;
+
+//     case 50:
+//         alert("Sizni yoshiz 50 da");
+//         break;
+
+//     default:
+//         alert("Sizni yoshiz nomalum");
+// }
+
+// function myFirstFunction(name) {
+//     console.log('Hi my name is Nurr');
+// }
+
+// myFirstFunction()
+
+// function myFirstFunction(name) {
+//     console.log(`Hi my name is ${name}`);
+// }
+// myFirstFunction("Nur");
+// myFirstFunction("Dos");
+// myFirstFunction("Ali");
+
+// //function decloration
+// myFirstFunction("Nur");
+// myFirstFunction("Ali");
+// function myFirstFunction(name) {
+//     console.log(`Hi my name is ${name}`);
+// }
+// //
+
+// //function expression
+// let myFunc = function () {
+//     console.log("This function is expression");
+// };
+// myFunc();
+
+// let num = 5;
+
+// function myFirstFunction(name) {
+//     let num = 10;
+//     num++;
+//     console.log(num);
+//     console.log(`Hi my name is ${name}`);
+// }
+// myFirstFunction("Nur");
+
+// console.log(num);
+
+///////////////////////////////////////////////////////////////////
+
+function myFirstApp(name, age) {
+    alert(`Hi my name is ${name} and i am full-stack developer`);
+
+    function showSkills() {
+        let skills = ["Html", "Css", "JavaScript"];
+        for (let i = 0; i < skills.length; i++) {
+            alert(`You have skills ${skills[i]}`);
+        }
+    }
+
+    showSkills();
+}
+
+// ////////////////////////////////////////////////////////////////
+
+// function checkAge() {
+//     // HOMEWORK???????????????????
+//     // age > 18 Exactly data for larning IT
+//     // age < 18 Super data for larning IT
+// }
+
+// checkAge();
+
+// ////////////////////////////////////////////////////////////////
+
+// function name (){
+//     return 'Hi my name is Nur'
+// }
+
+// console.log(name ());
+
+// ////////////////////////////////////////////////////////////////
+
+// function calcPow(num) {
+//     return num * num;
+// }
+// console.log(calcPow(5));
+
+// myFirstApp("Nur", 19);// hatolarrrrrr
+
+//////////////////////////////////////////////////////////////
+
+
+
+
